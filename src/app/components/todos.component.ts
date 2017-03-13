274ee029bf3beabd341281df0ba1cd87fdff6c02
Rafael Vieira <rz.vieira@outlook.com>
2017-03-12 22:19:07 -0300

    finalizacao

--- src/app/components/todos.component.ts
+++ src/app/components/todos.component.ts
@@ -10,25 +10,103 @@ import { ToDo } from '../models/todo';
 
 export class ToDosComponent implements OnInit {
 
-    public todos: ToDo[] = [];
-
-    ngOnInit(): void {
-        this.loadTodos();
-    }
+    public toDos: ToDo[] = [];
+    public inEditMode: boolean = false;
+    public currentToDo: ToDo;
+    public buttonLabel: string = 'Create';
 
     constructor(private service: ToDoService) {
+        this.currentToDo = new ToDo();
     }
 
-    private loadTodos() : void{
-        this.todos = [];
-        this.service.list()
+    ngOnInit(): void {
+        this.loadToDos();
+    }
+
+    public remove(id: number): void {
+        this.service.delete(id)
             .subscribe((res) => {
-               if(res.success){
-                   console.log(res.result);
-               } 
-               else{
-                   console.log(res.errors);
-               }
+                if (res.success) {
+                    this.loadToDos();
+                }
+                else {
+                    console.error(res.errors);
+                }
             })
+    }
+
+    public edit(toDo: ToDo): void {
+        this.currentToDo = toDo;
+        this.inEditMode = true;
+        this.buttonLabel = 'Save';
+    }
+
+    public setOrUnsetCompleted(toDo: ToDo): void {
+        toDo.isCompleted = !toDo.isCompleted;
+        this.service.put(toDo.id, toDo)
+            .subscribe((res) => {
+                if (!res.success) {
+                    console.error(res.errors);
+                }
+            });
+    }
+
+    public cancel(): void {
+        this.currentToDo = new ToDo();
+        this.inEditMode = false;
+    }
+
+    public save(): void {
+        //debugger
+        if (!this.inEditMode) {
+            this.saveNewToDo();
+        }
+        else {
+            this.updateToDo();
+        }
+    }
+
+    private saveNewToDo(): void {
+        this.service.post(this.currentToDo)
+            .subscribe((res) => {
+                if (res.success) {
+                    this.toDos.push(res.result);
+                    this.currentToDo = new ToDo();
+                    this.inEditMode = false;
+                    this.buttonLabel = 'Create';
+                }
+                else {
+                    console.error(res.errors);
+                }
+            });
+    }
+
+    private updateToDo(): void {
+        this.service.put(this.currentToDo.id, this.currentToDo)
+            .subscribe((res) => {
+                if (res.success) {
+                    this.currentToDo = new ToDo();
+                    this.inEditMode = false;
+                    this.buttonLabel = 'Create';
+                }
+                else {
+                    console.error(res.errors);
+                }
+            });
+    }
+
+
+    private loadToDos(): void {
+        this.toDos = [];
+        this.service.get()
+            .subscribe((res) => {
+                if (res.success) {
+                    this.toDos = res.result;
+                    console.log(res.result);
+                }
+                else {
+                    console.error(res.errors);
+                }
+            });
     }
 }
--- src/app/services/todos.service.ts
+++ src/app/services/todos.service.ts
@@ -12,10 +12,34 @@ export class ToDoService{
     private baseUrl: string = "http://localhost:57102/api/todo";    
 
     constructor(private http: Http){
-
     }
 
-    public list() : Observable<CommonResult<ToDo>>{
-        return this.http.get(this.baseUrl).map(res => res.json());
+    public geById(id: number) : Observable<CommonResult<ToDo>>{
+        return this.http.get(this.baseUrl + '/' + id)
+            .map(res => res.json());
+    }
+
+    public get() : Observable<CommonResult<ToDo[]>>{
+        return this.http.get(this.baseUrl)
+            .map(res => res.json());
+    }
+
+    public post(toDo: ToDo) : Observable<CommonResult<ToDo>>{
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this.http.post(this.baseUrl, JSON.stringify(toDo), { headers: headers })
+            .map(res => res.json());
+    }
+
+    public put(id: number, toDo: ToDo) : Observable<CommonSimpleResult>{
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        return this.http.put(this.baseUrl + '/' + id, JSON.stringify(toDo), { headers: headers })
+            .map(res => res.json());
+    }
+
+    public delete(id: number) : Observable<CommonSimpleResult>{
+        return this.http.delete(this.baseUrl + '/' + id)
+            .map(res => res.json());
     }
 }
